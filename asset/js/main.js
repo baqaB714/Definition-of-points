daf53922de67b30969d73a8f52684c23ad0adddf
abas basaki <ab.basaki@abfa.local>
2023-08-23 09:14:09 +0330

    after OK export xlsx 14020531

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,40 +1,44 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("export_XLX").addEventListener("click", function () {
+    const subNumber = document.querySelector("#sub_number").value;
+    const XBase = document.querySelector("#X_base").value;
+    const YBase = document.querySelector("#Y_base").value;
+    const landLength = document.querySelector("#land_length").value;
+    // const landWidth = document.querySelector('#land_width').value
 
+    const XZero = parseInt(XBase - parseInt(landLength) / 2);
+    const YZero = parseInt(YBase - parseInt(landLength) / 2);
+    const XEnd = XZero + parseInt(landLength);
 
-    document.getElementById('export_XLX').addEventListener('click', function () {
-        const subNumber = document.querySelector('#sub_number').value
-        const XBase = document.querySelector('#X_base').value
-        const YBase = document.querySelector('#Y_base').value
-        const landLength = document.querySelector('#land_length').value
-        // const landWidth = document.querySelector('#land_width').value
+    const numOfEachSide = parseInt(Math.round(Math.sqrt(subNumber)));
+    const distanceBWPoint = landLength / numOfEachSide;
 
-        const XZero = parseInt(XBase - (parseInt(landLength)/2));
-        const YZero = parseInt(YBase - (parseInt(landLength)/2));
-        const XEnd = XZero + parseInt(landLength)
+    let pointArr = [];
+    let newX = XZero;
+    let newY = YZero;
 
+    for (let i = 0; i <= subNumber; i++) {
+      if (newX < XEnd) {
+        newX += distanceBWPoint;
+        newY = newY;
+      } else {
+        newX = XZero;
+        newY += distanceBWPoint;
+      }
 
-        const numOfEachSide = parseInt(Math.round(Math.sqrt(subNumber)));
-        const distanceBWPoint = landLength / numOfEachSide
+      pointArr = [...pointArr, { x: newX, y: newY }];
+    }
 
-        let pointArr = [];
-        let newX = XZero;
-        let newY = YZero;
+    console.log(pointArr);
 
+    // convert object to csv-----------------
 
-        for (let i = 0; i <= subNumber; i++) {
+    var workbook2 = XLSX.utils.book_new(),
+      worksheet = XLSX.utils.json_to_sheet(pointArr);
+    workbook2.SheetNames.push("First");
+    workbook2.Sheets["First"] = worksheet;
 
-            if (newX < XEnd) {
-                newX += distanceBWPoint
-                newY = newY
-            } else {
-                newX = XZero
-                newY += distanceBWPoint
-            }
-
-            pointArr = [...pointArr , {x:newX , y : newY}]
-
-        }
-        console.log(pointArr);
-    })
-
-})
+    // download file ----------------------
+    XLSX.writeFile(workbook2, "demo.xlsx");
+  });
+});
